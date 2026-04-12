fbc83603a3e48fc8de4e0ce33ac1f6be0aeba635
priyashaS89 <priyashasubudhi2012@gmail.com>
2026-04-12 06:53:35 +0200

    Refactor MyPage class to include login method
    Replaced 'syao' method with 'login' method that prints a message multiple times.

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -4,8 +4,15 @@ package com.opencart.qa.pages;
 public class MyPage {
 	int age = 10;
 	String name = "Tom";
-	public void syao() {
-		System.out.println("Hello Page");
+
+	public void login() {
+		System.out.println("Hello App");
+		System.out.println("Hello App");
+		System.out.println("Hello App");
+		System.out.println("Hello App");
 	}
+}
+
+
 }
 
