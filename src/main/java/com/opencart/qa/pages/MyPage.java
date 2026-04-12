11d4f94ca7d852409281e7ea24089d76d5bc767f
priyashaS89 <priyashasubudhi2012@gmail.com>
2026-04-12 07:00:43 +0200

    Update MyPage.java

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -4,13 +4,6 @@ package com.opencart.qa.pages;
 public class MyPage {
 	int age = 10;
 	String name = "Tom";
-
-	public void login() {
-		System.out.println("Hello App");
-		System.out.println("Hello App");
-		System.out.println("Hello App");
-		System.out.println("Hello App");
-	}
 		public void logout() {
 		System.out.println("Hello logout");
 
